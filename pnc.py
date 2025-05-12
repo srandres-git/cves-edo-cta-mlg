cf6efbb3b81a63ede096b597c90e71e61e41c0f4
srandres-git <andysanchez2001@gmail.com>
2025-05-12 12:53:47 -0600

    fix: refactor preprocess_pnc to improve text processing and remove print statement

--- pnc.py
+++ pnc.py
@@ -9,8 +9,9 @@ def preprocess_pnc(uploaded_file)->pd.DataFrame:
     encoding = get_encoding(uploaded_file=uploaded_file)
     content = uploaded_file.read()               # bytes
     text = content.decode(encoding)              # str
-    print(text)
-    df = pd.read_csv(StringIO(text), encoding=encoding, sep=",")
+    data = text.splitlines()                    # lista de líneas
+    data_str = "\n".join(data)
+    df = pd.read_csv(StringIO(data_str), encoding=encoding, sep=",")
     # Reference a string sin "'" y sin espacios
     df["Reference"] = df["Reference"].astype(str).str.replace("'", "", regex=False).str.replace(" ", "", regex=False)
     
