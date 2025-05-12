63fa5c63751e12c4da23cbe67d6885664578f3e0
srandres-git <andysanchez2001@gmail.com>
2025-05-12 13:02:32 -0600

    fix: remove unused code and restore original file reading in preprocess_pnc function

--- pnc.py
+++ pnc.py
@@ -7,12 +7,13 @@ from utils import get_encoding
 def preprocess_pnc(uploaded_file)->pd.DataFrame:
     # para PNC, se recibe como .csv
     encoding = get_encoding(uploaded_file=uploaded_file)
-    content = uploaded_file.read()               # bytes
-    text = content.decode(encoding)              # str
-    data = text.splitlines()                    # lista de líneas
-    data_str = "\n".join(data)
-    print(data_str[:100])
-    df = pd.read_csv(StringIO(data_str), encoding=encoding, sep=",")
+    # content = uploaded_file.read()               # bytes
+    uploaded_file.seek(0)                        # reiniciar el puntero del archivo
+    # text = content.decode(encoding)              # str
+    # data = text.splitlines()                    # lista de líneas
+    # data_str = "\n".join(data)
+    # print(data_str[:100])
+    df = pd.read_csv(uploaded_file, encoding=encoding, sep=",")
     # Reference a string sin "'" y sin espacios
     df["Reference"] = df["Reference"].astype(str).str.replace("'", "", regex=False).str.replace(" ", "", regex=False)
     
