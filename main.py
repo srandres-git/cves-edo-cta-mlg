49bd2799f233a0b38129c4e3dae357b9fda066e2
srandres-git <andysanchez2001@gmail.com>
2025-05-12 13:19:26 -0600

    fix: update file uploader to accept multiple files for processing

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def load_file(uploaded_file):
 uploaded_files = st.file_uploader(
     "Arrastra uno o más archivos de estados de cuenta",
     type=['csv', 'xlsx', 'txt'],
-    accept_multiple_files=False,
+    accept_multiple_files=True
 )
 
 if uploaded_files:
